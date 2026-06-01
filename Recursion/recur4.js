921cf24207b112a63028a5ceb3cb12c12a6d5227
Jasiel Dsilva <dsilvajasiel@gmail..com>
2026-06-01 20:58:22 +0530

    array hash

--- Recursion/recur4.js
+++ Recursion/recur4.js
@@ -21,7 +21,7 @@ function reverse2(arr,i){
     [arr[i],arr[arr.length-1-i]] = [arr[arr.length-1-i],arr[i]]
     return reverse2(arr,i+1)
 }
-console.log(reverse2(arr,0))
+// console.log(reverse2(arr,0))
 
 var isPalindrome = function(s) {
     str = s.toLowerCase().replace(/[^a-z0-9]/g,"")
@@ -36,3 +36,11 @@ var isPalindrome = function(s) {
     }
     return check(0)
 };
+
+function fib(n){
+    if(n<=1){
+        return n
+    }
+    return fib(n-1)+fib(n-2)
+}
+console.log(fib(6))
---
+++ hashing/arr_hash.js
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 2, 4, 1, 2, 5, 12, 12];
+
+const hash = new Array(13).fill(0)
+
+for(let i=0;i<nums.length;i++){
+    hash[arr[i]]++
+}
+
+const queries = [1,2,12,6];
+
+for(let q of queries){
+    console.log(hash[q])
+}
