de983261adaef4ea203d0c78718cc718d8b18444
Jasiel Dsilva <dsilvajasiel@gmail.com>
2026-08-27 12:33:15 +0530

    count invrsions

---
+++ Array/CountInversions.java
@@ -0,0 +1,59 @@
+class Countinversions{
+    public static int inversions(int[] arr){
+        int count =0;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] > arr[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static int countInversionsOptimal(int[] arr){
+        return mergeSort(arr,0,arr.length-1);
+    }
+    private static int mergeSort(int[] arr, int low, int high){
+        int count = 0;
+        if(low>=high)return count;
+        int mid = low + (high - low)/2;
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid+1, high);
+        count += merge(arr,low,mid,high);
+        return count;
+       
+    }
+    private static  int merge(int[] arr, int low, int mid, int high){
+        int count = 0;
+        int left = low;
+        int right = mid+1;
+        int[] temp = new int[high-low+1];
+        int k = 0;
+        while(left <= mid && right<=high){
+            if(arr[left]<arr[right]){
+                temp[k] = arr[left];
+                left++;
+                k++;
+            }else{
+                temp[k] = arr[right];
+                count += mid-left+1;
+                k++;
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp[k++] = arr[left++];
+        }
+        while(right<=high){
+            temp[k++] = arr[right++];
+        }
+        for(int i=0;i<temp.length;i++){
+            arr[low+i] = temp[i];
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,3,2,4,1};
+        System.out.println(countInversionsOptimal(arr));
+    }
+}
